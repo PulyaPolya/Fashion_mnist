c1b525394b0b05c3334260e0ea28255ca770117c
PulyaPolya <polinaitsme@gmail.com>
2022-09-07 10:41:26 +0200

    augmentation

--- functions.py
+++ functions.py
@@ -8,6 +8,7 @@ from keras.datasets import mnist
 from keras.datasets import fashion_mnist
 from scipy.ndimage.interpolation import shift
 import random
+import matplotlib.pyplot as plt
 
 
 def edit_data(x_train, y_train, x_test, y_test):
@@ -161,33 +162,129 @@ def define_cnn_simplified(conv1, conv2,conv3, dropout1, dropout2):
     return model
 
 
-def shift_image(image, dx=10, dy=10):
-    if dx ==10 and dy ==10:
-        dx = random.randint(0, 1)
-        dy = random.randint(0, 1)
-    #image = image.reshape((28, 28))
+def shift_image(image, dx='NAN', dy='NAN'):
+    if dx =='NAN' or dy =='NAN':
+        dx, dy = random.choice([(1, 0), (-1, 0), (0, 1), (0, -1), (0, 0)])
     shifted_image = shift(image, [dy, dx], cval=0, mode="constant")
     return shifted_image
-def shift_image_np(x_batch):
-    # dx = random.randint(0, 1)
-    # dy = random.randint(0, 1)
-    dx, dy = random.choice([(1,0), (-1,0), (0,1), (0,-1), (0,0)])
-    # dx = random.choice([-1, 0,1])
-    # dy = random.choice([-1, 0, 1])
+def shift_batch_np(x_batch, dx ='nan', dy ='nan'):
+    if dx == 'nan'and dy == 'nan':
+        dx, dy = random.choice([(1,0), (-1,0), (0,1), (0,-1), (0,0)])
+    x_shifted = np.pad(x_batch, ((0, 0), (abs(dx), 0), (abs(dy), 0)), mode='constant')
+    x_shifted = np.roll(x_shifted, (dx, dy), axis=(1, 2))[:, abs(dx):, abs(dy):]
+    return x_shifted
+
+def shift_image_np(x_batch, dx ='nan', dy ='nan'):
+    if dx == 'nan'and dy == 'nan':
+        dx, dy = random.choice([(1,0), (-1,0), (0,1), (0,-1), (0,0)])
+    x_shifted = np.pad(x_batch, ((abs(dx), 0), (abs(dy), 0)), mode='constant')
+    x_shifted = np.roll(x_shifted, (dx, dy), axis=(0, 1))[abs(dx):, abs(dy):]
+    return x_shifted
+
+
+def shift_batch_not_random(x_batch,epoch_track='NAN', dx = 'NAN', dy = 'NAN', ):
+    #array = [(0,0),(1,0), (-1,0), (0,1), (0,-1)]
+    array = [(1,0), (-1,0), (0,1), (0,-1), (0,0)]
+    if epoch_track != "NAN":
+        #dx, dy = array[epoch_track.epoch%5]
+        dx, dy = random.choice(array)
+        if epoch_track.change:
+            print(dx, dy)
+            epoch_track.change = False
+    else:
+        x_batch = np.array(x_batch)
     x_shifted = np.pad(x_batch, ((0, 0), (abs(dx), 0), (abs(dy), 0)), mode='constant')
     x_shifted = np.roll(x_shifted, (dx, dy), axis=(1, 2))[:, abs(dx):, abs(dy):]
     return x_shifted
 
 def shift_x_train(x_train, y_train):
-    x_train_augmented = [image for image in x_train]
-    y_train_augmented = [image for image in y_train]
-    for dx, dy in ((1,0), (-1,0), (0,1), (0,-1)):
-         for image, label in zip(x_train, y_train):
-                 x_train_augmented.append(shift_image(image, dx, dy))
-                 y_train_augmented.append(label)
+    # x_train_augmented = [image for image in x_train]
+    # y_train_augmented = [image for image in y_train]
+    x_train_augmented = []
+    y_train_augmented= []
+    for image, label in zip(x_train, y_train):
+            for i in range (5):
+                dx, dy = random.choice([(0,0),(1, 0), (-1, 0), (0, 1), (0, -1)])
+                x_train_augmented.append(shift_image(image, dx, dy))
+                y_train_augmented.append(label)
+        # for dx, dy in ((1,0), (-1,0), (0,1), (0,-1)):
+        #      x_train_augmented.append(shift_image(image, dx, dy))
+        #      y_train_augmented.append(label)
     x_train_augmented = np.array(x_train_augmented)
     y_train_augmented = np.array(y_train_augmented)
+    x_train_augmented, y_train_augmented = shuffle(x_train_augmented, y_train_augmented)
     return x_train_augmented, y_train_augmented
 
+def shift_x_train_np_not_random(x_train, y_train):
+    # x_train_augmented = [image for image in x_train]
+    # y_train_augmented = [image for image in y_train]
+    x_train_augmented = []
+    y_train_augmented = []
+    array = [(1, 0), (-1, 0), (0, 1), (0, -1), (0, 0)]
+    i = 0
+    for i in range(5):
+        dx, dy = random.choice([(1, 0), (-1, 0), (0, 1), (0, -1), (0, 0)])
+    #for dx, dy in ((1,0), (-1,0), (0,1), (0,-1), (0,0)):
+
+        shift = list(shift_batch_not_random(x_train,'NAN', dx, dy))
+        x_train_augmented+= shift
+        y_train_augmented+= list(y_train)
+    # for image, label in zip(x_train, y_train):
+    #         x_train_augmented.append(shift_image_np(image, dx, dy))
+    #         y_train_augmented.append(label)
+    x_train_augmented = np.array(x_train_augmented)
+    y_train_augmented = np.array(y_train_augmented)
+    x_train_augmented, y_train_augmented = shuffle(x_train_augmented, y_train_augmented)
+    return x_train_augmented, y_train_augmented
+def shift_x_train1(x_train, y_train):
+        x_train_augmented = [image for image in x_train]
+        y_train_augmented = [image for image in y_train]
+        # x_train_augmented = []
+        # y_train_augmented =[]
+        for image, label in zip(x_train, y_train):
+            for dx, dy in ((1,0), (-1,0), (0,1), (0,-1)):
+                 for image, label in zip(x_train, y_train):
+                         x_train_augmented.append(shift_image(image, dx, dy))
+                         y_train_augmented.append(label)
+        x_train_augmented = np.array(x_train_augmented)
+        y_train_augmented = np.array(y_train_augmented)
+        x_train_augmented, y_train_augmented = shuffle(x_train_augmented, y_train_augmented)
+        return x_train_augmented, y_train_augmented
 def opposite(x):
     return 1-x
+
+
+def plot(x_train, size = 3, iterable = True):
+    if iterable:
+        for i in range(size):
+            plt.figure(figsize=(3, 3))
+            plt.imshow(x_train[i], cmap='binary')
+            plt.show()
+    else:
+        plt.figure(figsize=(3, 3))
+        plt.imshow(x_train, cmap='binary')
+        plt.show()
+
+
+
+def compare_arr(a,b):
+  wrong_indexes = []
+  s1, s2 = a.shape
+  for i in range (s1):
+    for j in range (s2):
+      if a[i][j] != b[i][j]:
+        wrong_indexes.append((i, j))
+  return wrong_indexes
+
+def arr_to_csv(arr, i):
+    df = pd.DataFrame(arr)
+    df.to_csv(f'arr_{i}.csv', mode='a', index=False, header=None)
+
+def save_results(type, iter, time, val_acc, test_acc, number):
+    if number == 1:
+        header = ['type', '#iter', 'time', 'val_acc', 'test_acc']
+    else:
+        header = None
+    df = pd.DataFrame([[type, iter, time, val_acc, test_acc]],
+                     index=[number], columns=['type', '#iter', 'time', 'val_acc', 'test_acc'])
+    df.to_csv("results.csv", mode='a', index=True, header=header)
--- test_model.py
+++ test_model.py
@@ -4,23 +4,33 @@ import functions as f
 from keras.callbacks import ModelCheckpoint, TensorBoard
 import time
 from tensorflow.keras import layers
+from keras.callbacks import LambdaCallback
 import matplotlib.pyplot as plt
 import numpy as np
 
+class Epoch_Tracker:
+  def __init__(self):
+     self.epoch = 0
+     self.change = True
+  def increase(self):
+    self.epoch +=1
+    self.change = True
+
+epoch_track = Epoch_Tracker()
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
-#x_train, y_train = f.shift_x_train(x_train, y_train)
+start = time.time()
+#x_train, y_train= f.shift_x_train_np_not_random(x_train, y_train)
 x_train, y_train, x_test, y_test = f.edit_data(x_train, y_train, x_test, y_test)
-# x_train = f.opposite(x_train)
-# x_test = f.opposite(x_test)
+
+#f.plot(x_train, 6)
+
 size_data = x_train.shape[0]
 batch_size = 64
 num_classes = 10
 num_iter =20000
 epochs =int ((batch_size / size_data) * num_iter)
-# plt.figure(figsize=(3, 3))
-# plt.imshow(x_train[0], cmap='binary')
-# plt.show()
+#epochs = 1
 
 NAME = "aug_np-1-{}".format(int(time.time()))
 
@@ -28,6 +38,9 @@ conv1=59
 conv2=33
 conv3=112
 def random_invert_img(x):
+  #print(epoch_track.epoch)
+  if epoch_track.epoch >= epochs:
+      return x
   x_temp = x.numpy()
   x_temp = x_temp.reshape(x_temp.shape[0], 28,28)
   # x_result = []
@@ -36,11 +49,25 @@ def random_invert_img(x):
   #     x_result.append(sing_ex)
   # x_result = np.array(x_result)
   #x_shifted = f.opposite(x_temp)
-  x_shifted = f.shift_image_np(x_temp)
-  # plt.figure(figsize=(3, 3))
-  # plt.imshow(x_shifted[0], cmap='binary')
-  # plt.show()
+  x_shifted = []
+  for image in x_temp:
+      x_shifted.append(f.shift_image_np(image))
+  x_shifted = np.array(x_shifted)
+  #x_shifted= f.shift_batch_not_random(x_temp,epoch_track)
+  #x_shifted = x_temp[:]
+  #f.plot(x_shifted[1], 1, iterable= False)
+  #f.plot( x_train_big.reshape(x_train_big.shape[0], 28,28)[0], 0, iterable  = False)
+
   x_result = x_shifted.reshape(x_temp.shape[0],28,28,1)
+  #print(np.array_equal(x_result[1], x_train_big[0]))
+  # shoe2 =  x_train_big[0].reshape(28,28)
+  # #print(x_result[1],x_train_big[0] )
+  # print(np.array_equal(shoe1, shoe2))
+  # print(np.allclose(shoe1, shoe2))
+  # print((shoe1 == shoe2).all())
+  # #print(shoe1, shoe2)
+  # print(f.compare_arr(shoe1, shoe2))
+
   return x_result
 def random_invert():
   return layers.Lambda(lambda x: random_invert_img(x))
@@ -54,7 +81,7 @@ class RandomInvert(layers.Layer):
     return random_invert_img(x)
 
 
-
+tf.random.set_seed(92)
 input_shape = (28, 28, 1)
 model = tf.keras.models.Sequential([
     RandomInvert(),
@@ -70,13 +97,15 @@ tensorboard = TensorBoard(log_dir='experiments/{}'.format(NAME), update_freq='ba
 #early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_acc', patience=10, baseline= 0.5)
 filepath = 'cnn_saved_model/aug_np_weights-improvement--{epoch:02d}-{val_loss:.2f}.hdf5'
 checkpoint = ModelCheckpoint(filepath, monitor = 'val_acc',verbose=1,  save_best_only=True, mode='max')
-callbacks_list = [checkpoint,tensorboard]
+num_ep = LambdaCallback(
+    on_epoch_end=lambda epoch,logs: epoch_track.increase())
+callbacks_list = [num_ep, checkpoint]
 
 
 
 model.compile(optimizer=tf.keras.optimizers.Adam(
     learning_rate=3e-4), loss='categorical_crossentropy', metrics=['acc'],  run_eagerly=True)
-start = time.time()
+
 history = model.fit(x_train, y_train,
                     batch_size=batch_size,
                     epochs=epochs,
@@ -88,3 +117,6 @@ end = time.time()
 print('Execution time:', time.strftime("%H:%M:%S", time.gmtime(end-start)))
 model.save("aug_np_model.h5")
 eval_metrics = model.evaluate(x_test, y_test)
+f.save_results(type='layer, shift_np,random, not vect, for each dx dy ', iter = 20000,
+               time = time.strftime("%H:%M:%S", time.gmtime(end-start)), val_acc =history.history['val_acc'][-1],
+               test_acc = eval_metrics[1], number=7)
