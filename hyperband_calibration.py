9e771a6967249f9c5274cafb266b7c215a4ce5d4
PulyaPolya <polinaitsme@gmail.com>
2023-06-01 10:50:53 +0200

    edited hyperband_calibration.py for oracle + fashion

--- hyperband_calibration.py
+++ hyperband_calibration.py
@@ -11,18 +11,21 @@ import oracle as oracle
 import random
 
 # dataset = 'FASHION'
-dataset = 'ORACLE'
-num_classes = 10
+# dataset = 'ORACLE'
+dataset = 'ORACLE+FASHION'
+
+x_train_orig, y_train_orig,  x_test_orig, y_test_orig = f.get_data_for_d_f()
+num_classes = 20
 input_shape = (28, 28, 1)
 # if dataset == 'FASHION':
 #     (x_train_orig, y_train_orig), (x_test_orig, y_test_orig) = fashion_mnist.load_data()
 # elif dataset == 'ORACLE':
 #     x_train_orig, y_train_orig, x_test_orig, y_test_orig = oracle.read_data_sets('oracle-mnist', one_hot=False, valid_num=0, dtype=tf.float32)
 # x_train, y_train, x_test, y_test = f.edit_data(x_train, y_train, x_test, y_test)
-x_train_orig, y_train_orig,  x_test_orig, y_test_orig = f.choose_dataset(dataset)
+# x_train_orig, y_train_orig,  x_test_orig, y_test_orig = f.choose_dataset(dataset)
 size_data = x_train_orig.shape[0]
 batch_size = 64
-epochs =10
+epochs =15
 class Epoch_Tracker:
   def __init__(self):
      self.epoch = 0
@@ -104,7 +107,7 @@ def run_search(NAME,x_train, y_train, x_val, y_val, max_epochs, num_of_runs):
                              objective='val_acc',
                              max_epochs=  max_epochs,
                              hyperband_iterations=1,
-                             directory='oracle/Hyperband',
+                             directory='oracle_and_fashion/Hyperband',
                              project_name=NAME)
 
 
@@ -120,17 +123,17 @@ def run_search(NAME,x_train, y_train, x_val, y_val, max_epochs, num_of_runs):
                                  kernel1=best_hps.get('kernel_size1'), kernel2=best_hps.get('kernel_size2'),
                                  kernel3=best_hps.get('kernel_size3'), opt=best_hps.get('optimizer'),
                                  dropout1=best_hps.get('drop1'), dropout2=best_hps.get('drop2'), val_acc='',
-                                 number=92, fold_numb=fold_numb, time= elapsed_time/3600, file_name='hyperband_oracle_results.csv')
+                                 number=92, fold_numb=fold_numb, time= elapsed_time/3600, file_name='hyperband_oracle+fashion_results.csv')
 
     # Get the optimal hyperparameters
-folds_numbers = ['1', '2', '3', '4', '5']
-# folds_numbers = ['1', '2', '3']
+# folds_numbers = ['1', '2', '3', '4', '5']
+folds_numbers = ['1', '2', '3']
 
 f.save_evolution_results(number_of_models = '' ,conv1='40-140', conv2='40-100', conv3='32-80', lr='5--15',
                          kernel1='3--7', kernel2='3--9', kernel3='3--15', opt='',
-                         dropout1='3--6',dropout2='3--6', val_acc='', number=0,fold_numb=0, time = 0, file_name = 'hyperband_oracle_results.csv')
-x_train_orig, y_train_orig, x_test_orig, y_test_orig = f.edit_data(x_train_orig, y_train_orig,
-                                                       x_test_orig, y_test_orig)
+                         dropout1='3--6',dropout2='3--6', val_acc='', number=0,fold_numb=0, time = 0, file_name = 'hyperband_oracle+fashion_results.csv')
+# x_train_orig, y_train_orig, x_test_orig, y_test_orig = f.edit_data(x_train_orig, y_train_orig,
+#                                                        x_test_orig, y_test_orig)
 
 
 if dataset == 'FASHION':
@@ -164,32 +167,28 @@ if dataset == 'FASHION':
         NAME = "Hyperband_fold" + fold_numb
         run_search(NAME, x_train, y_train, x_val, y_val, num_of_runs=1)
 
-elif dataset == 'ORACLE':
+else:
     folds_train, folds_labels = f.split_dataset(dataset, x_train_orig, y_train_orig)
-    # folds_train = np.array_split(x_train_orig[:-2], 5)
-    # folds_labels = np.array_split(y_train_orig[:-2], 5)
-    # n1 = random.choice([0,1,2,3,4])
-    # folds_train[n1]=np.concatenate((folds_train[n1], np.expand_dims(x_train_orig[-2], axis=0)) )
-    # folds_labels[n1] = np.concatenate((folds_labels[n1], np.expand_dims(y_train_orig[-2], axis=0)) )
-    # n2 = random.choice([0,1,2,3,4])
-    # folds_train[n2] = np.concatenate((folds_train[n2], np.expand_dims(x_train_orig[-1], axis=0)))
-    # folds_labels[n2] = np.concatenate((folds_labels[n2], np.expand_dims(y_train_orig[-1], axis=0)) )
     for fold_numb in folds_numbers:
         if fold_numb == '1':
             x_train = np.concatenate((folds_train[1], folds_train[2], folds_train[3], folds_train[4]))
             y_train = np.concatenate((folds_labels[1],folds_labels[2], folds_labels[3], folds_labels[4]))
             x_val = folds_train[0]
             y_val = folds_labels[0]
+            max_epochs = 15
+
         elif fold_numb == '2':
             x_train = np.concatenate((folds_train[0], folds_train[2], folds_train[3], folds_train[4]))
             y_train = np.concatenate((folds_labels[0],folds_labels[2], folds_labels[3], folds_labels[4]))
             x_val = folds_train[1]
             y_val = folds_labels[1]
+            max_epochs = 20
         elif fold_numb == '3':
             x_train = np.concatenate((folds_train[0], folds_train[1], folds_train[3], folds_train[4]))
             y_train = np.concatenate((folds_labels[0],folds_labels[1], folds_labels[3], folds_labels[4]))
             x_val = folds_train[2]
             y_val = folds_labels[2]
+            max_epochs = 25
         elif fold_numb == '4':
             x_train = np.concatenate((folds_train[0], folds_train[1], folds_train[2], folds_train[4]))
             y_train = np.concatenate((folds_labels[0],folds_labels[1], folds_labels[2], folds_labels[4]))
@@ -200,7 +199,7 @@ elif dataset == 'ORACLE':
             y_train = np.concatenate((folds_labels[0],folds_labels[1], folds_labels[2], folds_labels[3]))
             x_val = folds_train[4]
             y_val = folds_labels[4]
-        max_epochs = 70
+
         print(f'\n training for the fold number {fold_numb} \n')
         NAME = "Hyperband_fold" + fold_numb
         run_search(NAME, x_train, y_train, x_val, y_val, max_epochs, num_of_runs=1)
