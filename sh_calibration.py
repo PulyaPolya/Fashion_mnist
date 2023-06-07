b6bec9b388ff23046a7d865c3c51d59b351271f1
PulyaPolya <polinaitsme@gmail.com>
2023-06-07 22:08:19 +0200

    edited sh_calibration.py for oracle + fashion

--- sh_calibration.py
+++ sh_calibration.py
@@ -9,15 +9,16 @@ import keras_tuner as kt
 from tensorflow import keras
 import oracle as oracle
 
-dataset = 'ORACLE'
+dataset = 'ORACLE+FASHION'
 # dataset = 'FASHION'
-num_classes = 10
+num_classes = 20
 input_shape = (28, 28, 1)
-x_train_orig, y_train_orig,  x_test_orig, y_test_orig = f.choose_dataset(dataset)
+# x_train_orig, y_train_orig,  x_test_orig, y_test_orig = f.choose_dataset(dataset)
 # x_train, y_train, x_test, y_test = f.edit_data(x_train, y_train, x_test, y_test)
+x_train_orig, y_train_orig,  x_test_orig, y_test_orig = f.get_data_for_d_f()
 size_data = x_train_orig.shape[0]
 batch_size = 64
-epochs =10
+epochs =15
 class Epoch_Tracker:
   def __init__(self):
      self.epoch = 0
@@ -99,7 +100,7 @@ def run_search(NAME,x_train, y_train, x_val, y_val, num_of_runs, max_epochs):
                              objective='val_acc',
                              max_epochs=  max_epochs,
                              hyperband_iterations=1,
-                             directory='oracle/SH',
+                             directory='oracle+fashion/SH',
                              project_name=NAME)
 
 
@@ -115,16 +116,16 @@ def run_search(NAME,x_train, y_train, x_val, y_val, num_of_runs, max_epochs):
                                  kernel1=best_hps.get('kernel_size1'), kernel2=best_hps.get('kernel_size2'),
                                  kernel3=best_hps.get('kernel_size3'), opt=best_hps.get('optimizer'),
                                  dropout1=best_hps.get('drop1'), dropout2=best_hps.get('drop2'), val_acc='',
-                                 number=92, fold_numb=fold_numb, time= elapsed_time/3600, file_name='sh_oracle_results.csv')
+                                 number=92, fold_numb=fold_numb, time= elapsed_time/3600, file_name='sh_oracle+fashion_results.csv')
 
     # Get the optimal hyperparameters
-folds_numbers = ['1', '2', '3', '4', '5']
-# folds_numbers = ['1']
+# folds_numbers = ['1', '2', '3', '4', '5']
+folds_numbers = ['1']
 f.save_evolution_results(number_of_models = '' ,conv1='40-140', conv2='40-100', conv3='32-80', lr='5--15',
                          kernel1='3--7', kernel2='3--9', kernel3='3--15', opt='',
-                         dropout1='3--6',dropout2='3--6', val_acc='', number=0,fold_numb=0, time = 0, file_name = 'sh_oracle_results.csv')
-x_train_orig, y_train_orig, x_test_orig, y_test_orig = f.edit_data(x_train_orig, y_train_orig,
-                                                       x_test_orig, y_test_orig)
+                         dropout1='3--6',dropout2='3--6', val_acc='', number=0,fold_numb=0, time = 0, file_name = 'sh_oracle+fashion_results.csv')
+# x_train_orig, y_train_orig, x_test_orig, y_test_orig = f.edit_data(x_train_orig, y_train_orig,
+#                                                        x_test_orig, y_test_orig)
 if dataset == 'FASHION':
     for fold_numb in folds_numbers:
         if fold_numb == '1':
@@ -162,7 +163,7 @@ if dataset == 'FASHION':
         NAME = "SH_fold" + fold_numb
         run_search(NAME, x_train, y_train, x_val, y_val, 1, max_epochs)
 
-elif dataset == 'ORACLE':
+else:
     folds_train, folds_labels = f.split_dataset(dataset, x_train_orig, y_train_orig)
     for fold_numb in folds_numbers:
         if fold_numb == '1':
@@ -170,35 +171,35 @@ elif dataset == 'ORACLE':
             y_train = np.concatenate((folds_labels[1],folds_labels[2], folds_labels[3], folds_labels[4]))
             x_val = folds_train[0]
             y_val = folds_labels[0]
-            # x_val = x_train_orig[-100:]
-            # y_val = y_train_orig[-100:]
-            # x_train = x_train_orig[:100]
-            # y_train = y_train_orig[:100]
-            max_epochs = 180
+            # x_val = x_train_orig[-70:]
+            # y_val = y_train_orig[-70:]
+            # x_train = x_train_orig[:70]
+            # y_train = y_train_orig[:70]
+            max_epochs = 50
         elif fold_numb == '2':
             x_train = np.concatenate((folds_train[0], folds_train[2], folds_train[3], folds_train[4]))
             y_train = np.concatenate((folds_labels[0],folds_labels[2], folds_labels[3], folds_labels[4]))
             x_val = folds_train[1]
             y_val = folds_labels[1]
-            max_epochs = 190
+            max_epochs = 60
         elif fold_numb == '3':
             x_train = np.concatenate((folds_train[0], folds_train[1], folds_train[3], folds_train[4]))
             y_train = np.concatenate((folds_labels[0],folds_labels[1], folds_labels[3], folds_labels[4]))
             x_val = folds_train[2]
             y_val = folds_labels[2]
-            max_epochs = 200
+            max_epochs =70
         elif fold_numb == '4':
             x_train = np.concatenate((folds_train[0], folds_train[1], folds_train[2], folds_train[4]))
             y_train = np.concatenate((folds_labels[0],folds_labels[1], folds_labels[2], folds_labels[4]))
             x_val = folds_train[3]
             y_val = folds_labels[3]
-            max_epochs = 210
+            max_epochs = 80
         elif fold_numb == '5':
             x_train = np.concatenate((folds_train[0], folds_train[1], folds_train[2], folds_train[3]))
             y_train = np.concatenate((folds_labels[0],folds_labels[1], folds_labels[2], folds_labels[3]))
             x_val = folds_train[4]
             y_val = folds_labels[4]
-            max_epochs =220
+            max_epochs =90
         print(f'\n training for the fold number {fold_numb} \n')
         NAME = "SH_fold" + fold_numb
         run_search(NAME, x_train, y_train, x_val, y_val, 1, max_epochs)
